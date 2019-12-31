f48f1ebded31d39cad1bf8023d148e6e25ce6be3
Cici Jiayi Shen <jiayis.18@intl.zju.edu.cn>
2019-12-31 14:47:06 +0800

    Add review 8 & 9

--- Problem_Sets/9/Problem_Set_9_sample_code.R
+++ Problem_Sets/9/Problem_Set_9_sample_code.R
@@ -1,17 +1,22 @@
 temp <- readtxt("C:/Users/sissy/Desktop/test Git/ADS2_2019-21/Problem_Sets/9/barley.txt")
 barley <- scan("C:/Users/sissy/Desktop/test Git/ADS2_2019-21/Problem_Sets/9/barley.txt")
 t.test(barley,mu = 50)
+
+# first assumption: records are continuous and random.
 head(barley)
 
+# second assumption: sampling distribution is normally distributed.
+# because each sample is the same length as the original data, I need to use replace = TRUE or all samples will just match the original data.
 sampling_means<-vector()
 for (replicate in 1:100){
   barley_sample<-sample(barley, size = length(barley), replace = TRUE)
   sampling_means<-c(sampling_means, mean(barley_sample))
 }
 hist(sampling_means, xlab = "Sample means", main = "")
-
+# this can be tested using Shapiro-Wilks test, which is a statistical test of normality. In this test, the null hypothesis is that the data is normally distributed.
 shapiro.test(sampling_means)
 
+# third assumption: mean and sd are independent.
 sampling_errors<-vector()
 sampling_means<-vector()
 for (replicate in 1:100){
